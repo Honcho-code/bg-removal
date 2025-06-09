633149ed1f0b487450c30892755d3469d3fd3e97
Rapheal <clintonrapheal5@gmail.com>
2025-06-09 03:35:07 +0100

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+import BgSlider from '../components/BgSlider'
+import Testemonials from '../components/Testemonials'
+import Uplod from '../components/Uplod'
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <Steps/>
+      <BgSlider/>
+      <Testemonials/>
+      <Uplod/>
+    </div>
+  )
+}
+
+export default Home
